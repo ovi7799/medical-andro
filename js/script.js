9c4588446328ae71e978cb58f189a448fd80c642
Ovi <ovihossain7799@gmail.com>
2024-10-01 13:18:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(".count").each(function () {
+    $(this)
+        .prop("Counter", 0)
+        .animate(
+            {
+                Counter: $(this).text(),
+            },
+            {
+                duration: 2000,
+                easing: "swing",
+                step: function (now) {
+                    now = Number(Math.ceil(now)).toLocaleString('en');
+                    $(this).text(now);
+                },
+            }
+        );
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
